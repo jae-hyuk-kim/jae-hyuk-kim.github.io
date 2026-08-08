041347ceb7cf3d6c7669d6fb243d242a6fe58d8f
jae-hyuk-kim <58111559+jae-hyuk-kim@users.noreply.github.com>
2026-08-08 00:27:38 +0000

    Deploying to gh-pages from @ jae-hyuk-kim/jae-hyuk-kim.github.io@22fac2d6f02950ff02c5ee88f637468be0573bd2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,7 +476,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=https://scholar.google.com/citations?hl=ko&user=1CtgrgEAAAAJ&view_op=list_works&sortby=pubdate", "_blank");
         },
       },{
         id: 'social-custom_social',
